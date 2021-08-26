2cd3f57b6b052a5574f2a126af0c396c556f2062
Andres <andres.paredes@protonmail.com>
2021-08-26 17:27:39 -0400

    movies excerse 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Movie Photos';//'Photo/Image Section Title Here';
+  image1 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/The_Manchurian_Candidate_%281962_poster%29.jpg/314px-The_Manchurian_Candidate_%281962_poster%29.jpg';
+  image2 = 'https://upload.wikimedia.org/wikipedia/en/d/dd/OceansEightPoster.jpeg';
+  image3 = 'https://upload.wikimedia.org/wikipedia/en/e/ec/The_Incredibles.jpg';
+  image4 = 'https://upload.wikimedia.org/wikipedia/en/4/4f/The_official_poster_for_the_film_Hidden_Figures%2C_2016.jpg';
+
+  image1Title = "The Manchurian Candidate";
+  image2Title = "Oceans 8";
+  image3Title = "The Incredibles";
+  image4Title = "Hidden Figures";
 
   constructor() { }
 
